791310769561e4d7980a09dc0c4681f352f7f44c
panipanipa <denis@gouvine.fr>
2021-01-16 12:18:01 +0100

    import ok

--- neo4j-java-bolt/src/main/java/example/movies/backend/DatabaseService.java
+++ neo4j-java-bolt/src/main/java/example/movies/backend/DatabaseService.java
@@ -1,9 +1,11 @@
 package example.movies.backend;
 
+import javafx.util.Pair;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.Session;
 import org.neo4j.driver.SessionConfig;
 import org.neo4j.driver.Value;
+
 import spark.Spark;
 
 import java.io.File;
@@ -15,18 +17,75 @@ public class DatabaseService {
 
     private final String database;
 
+    public enum type {INTEGER, FLOAT, STRING;} ;
+
+
     public DatabaseService(Driver driver, String database) {
         this.driver = driver;
         this.database = database;
         //Spark.ipAddress("10.29.40.63");
     }
 
-    public void importCSV(String path, String sepa, boolean local) {
+    public void importNodeCSV(String path, String sepa, boolean local, String index, List<Pair<String, type>> fields) {
         System.out.println("import CSV");
         String dest = local ? "'file://"+path+"'" : path ;
+        StringBuilder result = new StringBuilder("(:" + index + " { ");
+        int i = 0 ;
+        for (Pair<String, type> field: fields) {
+            if(i>0)
+                result.append(", " );
+            switch (field.getValue()) {
+                case INTEGER:
+                    result.append(field.getKey()).append(": toInteger(line[").append(i).append("])");
+                    break ;
+
+                case STRING:
+                    result.append(field.getKey()).append(": line[").append(i).append("]");
+                    break ;
+
+                case FLOAT:
+                    result.append(field.getKey()).append(": toFloat((line[").append(i).append("])");
+                    break ;
+            }
+            i++ ;
+        }
+        result.append("})") ;
         String query = "LOAD CSV FROM "+ dest +" AS line " +
                 "FIELDTERMINATOR '" + sepa  + "' " +
-                "CREATE (:Email { name: toInteger(line[1]), department: toInteger(line[2])}) " ;
+                "CREATE " + result.toString() ;
+        System.out.println(query) ;
+        write_query(query) ;
+    }
+
+    private String convertType(type t, int i) {
+        StringBuilder result = new StringBuilder("");
+        switch (t) {
+            case INTEGER:
+                result.append("toInteger(line[").append(i).append("])");
+                break ;
+
+            case STRING:
+                result.append(": line[").append(i).append("]");
+                break ;
+
+            case FLOAT:
+                result.append(": toFloat((line[").append(i).append("])");
+                break ;
+        }
+        return result.toString() ;
+    }
+
+    public void importEdgeCSV(String path, String sepa, boolean local, String index, String edge ,Pair< Pair<String, type>, Pair<String,type> > fields) {
+        System.out.println("import CSV");
+        String dest = local ? "'file://"+path+"'" : path ;
+        String match1 = "Match (s:"+index + " { " + fields.getKey().getKey() + ":" + convertType(fields.getKey().getValue(), 0) + "})" ;
+        String match2 = ",(e:"+index + " { " + fields.getValue().getKey() + ":" + convertType(fields.getValue().getValue(), 1) + "})" ;
+        StringBuilder result = new StringBuilder("");
+        String relation = "Create (s)-[:"+edge+"]->(e)" ;
+        String query = "LOAD CSV FROM "+ dest +" AS line " +
+                "FIELDTERMINATOR '" + sepa  + "' " +
+                match1 + match2 + " " + relation ;
+        System.out.println(query) ;
         write_query(query) ;
     }
 
